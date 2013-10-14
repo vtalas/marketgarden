9d9dfde50a0caa58ee10d6ae83e39650656167b0
v.talas <v.talas@gmc.net>
2013-10-14 07:31:43 +0200

    aaaa

---
+++ marketGardenApi/Controllers/homeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MarketGarden;
+using marketGarden;
+using MarketGarden.PathResolver;
+using MarketGarden.Readers;
+using MarketGarden.Recorder;
+using MarketGarden.RecordProcessor;
+
+namespace marketGardenApi.Controllers
+{
+    public class HomeController : Controller
+    {
+		PicusWithName MarketInfoRecorderFactory(IMarketReader reader, string marketName, string baseSymbol, string altSymbol)
+		{
+			var pathResolver = new PathResolverWeb(baseSymbol, altSymbol, marketName, System.Web.HttpContext.Current);
+			var settings = new MarketDataSettings
+			{
+				MarketReader = reader,
+				SymbolBase = baseSymbol,
+				SymbolAlt = altSymbol,
+				MarketRecordProcessor = new TsvFileWriter(pathResolver)
+			};
+
+			return new MarketInfoRecorder(settings).Process(marketName);
+
+		}
+		public ActionResult Index()
+        {
+			const string baseSymbol = "LTC";
+	        const string altSymbol = "BTC";
+
+			var x = new List<PicusWithName>
+			{
+				MarketInfoRecorderFactory(new BtceReader(), "btce", baseSymbol, altSymbol),
+				MarketInfoRecorderFactory(new CryptoTradeReader(), "crypto", baseSymbol, altSymbol),
+				MarketInfoRecorderFactory(new VirCurexReader(), "vircurex", baseSymbol, altSymbol),
+				MarketInfoRecorderFactory(new McxReader(), "mcx", baseSymbol, altSymbol)
+			};
+
+			var pathResolver = new PathResolverWeb(baseSymbol, altSymbol, "diff", System.Web.HttpContext.Current);
+			var cho = new Chochoo(x, new TsvFileWriter(pathResolver));
+			cho.Process();
+
+			return View();
+        }
+
+    }
+}
